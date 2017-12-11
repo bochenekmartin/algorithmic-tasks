c57af42795142061c5514c18ab2296b7d8e7f847
Martin_Bochenek <bochenek.martin@gmail.com>
2017-12-11 15:30:20 +0100

    Removed not used comment

--- src/main/java/com/algorythmic/tasks/problems/projecteuler/Problem001.java
+++ src/main/java/com/algorythmic/tasks/problems/projecteuler/Problem001.java
@@ -1,9 +1,5 @@
 package com.algorythmic.tasks.problems.projecteuler;
 
-/**
- * User: Martin Bochenek (SG0212051)
- * Date: Oct 21, 2010
- */
 public class Problem001 {
 
     public static void main(String... args) {
